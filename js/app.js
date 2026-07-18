f698483305ec0d8787848fde3fd95128f7939391
Ashraf <ashrafunnisa135@gmail.com>
2026-07-19 04:27:04 +1000

    edit paye layout and add sidebar links icons

--- js/app.js
+++
@@ -1,7 +0,0 @@
-import { initSidebar } from "./components/sidebar.js";
-import { initNavbar } from "./components/navbar.js";
-
-document.addEventListener("DOMContentLoaded", () => {
-  initSidebar();
-  initNavbar();
-});
